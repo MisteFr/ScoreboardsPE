1aa1b47e22142aefac6b67c00a9263b1c5549fc4
Miste <misteboss1@gmail.com>
2018-10-17 19:40:55 +0200

    That doesn't need to be here. I will make a demo plugin later !

--- src/Miste/scoreboardspe/EventHandler.php
+++ src/Miste/scoreboardspe/EventHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Miste\scoreboardspe;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerChatEvent;
 
 use Miste\scoreboardspe\API\{
@@ -20,16 +19,6 @@ class EventHandler implements Listener{
 
 	public function __construct(ScoreboardsPE $plugin){
 		$this->plugin = $plugin;
-	}
-
-	public function onPlayerJoinEvent(PlayerJoinEvent $event){
-		$scoreboard = new Scoreboard($this->plugin, "Miste", ScoreboardAction::CREATE);
-		$scoreboard->create(ScoreboardDisplaySlot::SIDEBAR, ScoreboardSort::DESCENDING);
-
-		$scoreboard->addDisplay($event->getPlayer());
-		for($i = 1; $i <= 15; $i++){
-			$scoreboard->setLine($event->getPlayer(), $i, "line$i");
-		}
 	}
 
 	public function onQuitEvent(PlayerQuitEvent $event){
